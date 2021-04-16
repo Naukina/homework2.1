656ffee593accea938741ae11dcebf464f76df75
Aidana Naukina <naukina_aidana@mail.ru>
2021-04-16 19:04:00 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 21_200;
+        int expense = 20;
+        int totalMiles = price / expense;
+        System.out.println(totalMiles);
+    }
+}
